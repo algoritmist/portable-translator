37c4a1b2b0f8c077eed45bf9103e4428ea937e47
Viacheslav Baranov <barash.2018@yandex.ru>
2024-07-23 15:22:44 +0800

    feat: add text-to-speech

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -2,6 +2,7 @@ import whisper
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 
+
 app = FastAPI()
 app_host = "0.0.0.0"
 app_port = 8000
@@ -19,12 +20,14 @@ async def index():
 @app.post("/transcribe")
 async def trascribe(audio_file: UploadFile = File()):
     contents = await audio_file.read()
+    # TODO: should use other approach for multi-user support
     temp_filename = "temp_file.flac.temp"
     with open(temp_filename, "wb") as f:
         f.write(contents)
         f.close()
     result = tiny_model.transcribe(temp_filename)
     return result
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=app_host, port=app_port, reload=True)
--- client/client/main.py
+++ client/client/main.py
@@ -1,9 +1,18 @@
 import requests
+import pyttsx3
 
-url="http://0.0.0.0:8000/transcribe"
+url_home = "http://0.0.0.0:8000/"
+url_transcribe = url_home + "transcribe/"
+#url_speak = url_home + "speak/"
 
 if __name__ == "__main__":
     audio_file = "jfk.flac"
     with open(audio_file, 'rb') as audio:
-        responce = requests.post(url, files={"audio_file" : audio})
-        print(responce.json())
+        responce_transcribe = requests.post(url_transcribe, files={"audio_file" : audio})
+        print(responce_transcribe.json())
+        text = responce_transcribe.json()["text"]
+        engine = pyttsx3.init()
+        engine.setProperty("volume", 1.0)
+        engine.say(text)
+        engine.runAndWait()
+        engine.stop()
