445c1cfe51b8ef260e5ee851ea835e7f2226f8af
Viacheslav Baranov <barash.2018@yandex.ru>
2024-07-23 14:32:23 +0800

    fix: temp filename store & load

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -19,9 +19,12 @@ async def index():
 @app.post("/transcribe")
 async def trascribe(audio_file: UploadFile = File()):
     contents = await audio_file.read()
-    with open("temp_file.flac.temp", "wb") as f:
+    temp_filename = "temp_file.flac.temp"
+    with open(temp_filename, "wb") as f:
         f.write(contents)
-        return tiny_model.transcribe(f)
+        f.close()
+    result = tiny_model.transcribe(temp_filename)
+    return result
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=app_host, port=app_port, reload=True)
