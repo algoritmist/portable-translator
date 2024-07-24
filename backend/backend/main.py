454328f5f12f584dec56e7aaffddfb22b530ec27
Viacheslav Baranov <barash.2018@yandex.ru>
2024-07-24 11:55:58 +0800

    feat: add traslation

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app_host = "0.0.0.0"
 app_port = 8000
 
-tiny_model = whisper.load_model("tiny")
+tiny_model = whisper.load_model("medium")
 
 
 @app.get("/")
@@ -17,15 +17,16 @@ async def index():
     """
     return {"message" : "Speech-to-text server"}
 
-@app.post("/transcribe")
-async def trascribe(audio_file: UploadFile = File()):
+@app.post("/transcribe/{language_to}")
+async def trascribe(language_to: str, audio_file: UploadFile = File()):
     contents = await audio_file.read()
     # TODO: should use other approach for multi-user support
     temp_filename = "temp_file.flac.temp"
     with open(temp_filename, "wb") as f:
         f.write(contents)
         f.close()
-    result = tiny_model.transcribe(temp_filename)
+    options = dict(language=language_to)
+    result = tiny_model.transcribe(temp_filename, **options)
     return result
 
 
--- client/client/main.py
+++ client/client/main.py
@@ -3,14 +3,16 @@ import pyttsx3
 
 url_home = "http://0.0.0.0:8000/"
 url_transcribe = url_home + "transcribe/"
+url_transcribe_gr = url_transcribe + "greek"
 #url_speak = url_home + "speak/"
 
 if __name__ == "__main__":
     audio_file = "jfk.flac"
     with open(audio_file, 'rb') as audio:
-        responce_transcribe = requests.post(url_transcribe, files={"audio_file" : audio})
+        responce_transcribe = requests.post(url_transcribe_gr, files={"audio_file" : audio})
         print(responce_transcribe.json())
         text = responce_transcribe.json()["text"]
+        print(text)
         engine = pyttsx3.init()
         engine.setProperty("volume", 1.0)
         engine.say(text)
