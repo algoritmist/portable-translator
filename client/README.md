a7ea2b69f30a8ebca066ac56b471a8f7bc77d529
Viacheslav Baranov <barash.2018@yandex.ru>
2024-07-25 19:06:57 +0800

    feat: implement english to french tts example

--- client/README.md
+++ client/README.md
@@ -1,3 +1,5 @@
 # Text-to-speech client example
 ## Build
 To build the project run ```poetry build```
+## TTS library
+If you encounter problems downloading the model directly from ```pip```, you can download it from github and put in ```~/.local/share/tts/``` and change config and speaker paths in ```config.json```
--- client/client/main.py
+++ client/client/main.py
@@ -1,6 +1,5 @@
 import requests
 from TTS.api import TTS
-from TTS.utils.synthesizer import Synthesizer
 import torch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -8,21 +7,25 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 url_home = "http://0.0.0.0:8000/"
 url_transcribe = url_home + "transcribe/"
 url_transcribe_en = url_transcribe + "english"
-url_transcribe_gr = url_transcribe + "greek"
+url_transcribe_fr = url_transcribe + "french"
 #url_speak = url_home + "speak/"
 
-synthesizer = Synthesizer()
-
 tts_model = "tts_models/multilingual/multi-dataset/your_tts"
-vocoder_model = "vocoder_models/universal/libri-tts/wavegrad"
 
+""" Working tts model paths if you want to add them manualy. Be sure to plase them in ~/.local/share/tts, otherwise you can encounter bugs in api path resolving
+tts_path = "~/.local/share/tts/tts_models--multilingual--multi-dataset--your_tts/tts_models/model_file.pth"
+config_path = "~/.local/share/tts/tts_models/tts_models--multilingual--multi-dataset--your_tts/config.json"
+"""
+
+#tts = TTS(model_path=tts_path, config_path=config_path).to("cpu")
 tts = TTS(model_name=tts_model)
 
 if __name__ == "__main__":
     audio_file = "jfk.flac"
     with open(audio_file, 'rb') as audio:
-        responce_transcribe = requests.post(url_transcribe_gr, files={"audio_file" : audio})
+        responce_transcribe = requests.post(url_transcribe_fr, files={"audio_file" : audio})
         audio.close()
         print(responce_transcribe.json())
         text = responce_transcribe.json()["text"]
-        tts.tts_to_file(text=text, file_path="tts_out_file.wav")
+        tts.tts_to_file(text=text, language="fr-fr", speaker=tts.speakers[0], file_path="tts_out_file.wav")
+        #wav = tts.tts(text=text, file_path="tts_out_file.wav")
